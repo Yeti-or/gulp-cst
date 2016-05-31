d8d18a0f3fbec28132a891c270330a244c2e99d9
Vasiliy <Yeti.or@gmail.com>
2016-05-31 09:32:33 +0300

    Fix indention

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var through = require('through2');
 var Parser = require('cst').Parser;
 
 module.exports = function(options) {
-	var parser = new Parser(options);
+    var parser = new Parser(options);
     return through.obj(function(file, enc, next) {
         var fileContent = file.contents.toString(enc);
         file.tree = parser.parse(fileContent);
